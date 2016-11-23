281e3efcba8d897ca6d0d0d5ffac0eb962bdfb2d
RobQuatela <rob.quatela@gmail.com>
2016-11-23 16:40:59 -0500

    11/23/16

--- src/com/robquatela/calcengine/Main.java
+++ src/com/robquatela/calcengine/Main.java
@@ -13,6 +13,19 @@ public class Main {
         equations[2] = new MathEquation('s', 225.0d, 17.0d);
         equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
+        String[] statements = {
+                "Divide 100.0 50.0",
+                "Add 25.0 92.0",
+                "Subtract 225.0 17.0",
+                "Multiply 11.0 3.0"
+        };
+
+        CalculateHelper helper = new CalculateHelper();
+        for(String statement: statements) {
+            helper.process(statement);
+            System.out.println(helper);
+        }
+
         for(MathEquation equation : equations) {
             equation.execute();
             System.out.printf("result = %.1f", equation.getResult());
@@ -37,6 +50,23 @@ public class Main {
 
         equationOverload.execute((double)leftInt, rightInt);
         System.out.printf("Result = %.1f\n", equationOverload.getResult());
+
+
+        System.out.println();
+        System.out.println("Using Inheritance");
+        System.out.println();
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d, 50.d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d, 17.0d),
+                new Multiplier(11.0d, 3.0d)
+        };
+
+        for(CalculateBase calculator:calculators) {
+            calculator.calculate();
+            System.out.println("Result = " + calculator.getResult());
+        }
     }
 
 
